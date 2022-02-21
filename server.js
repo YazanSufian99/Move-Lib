3cfee9d7439ad212b0c7de185acd3ca9958bd199
YazanSufian99 <yazan.sufian@yahoo.com>
2022-02-21 20:50:32 +0200

    task 12

--- server.js
+++ server.js
@@ -3,14 +3,46 @@ const express= require("express");
 const movies =require("./data.json");
 const app =express();
 
-function Movie(title, overview, poster_path){
+// day 12 updates
+const axios =require("axios");
+/*
+we create .env file to store api key inside him to make
+sure our api key is peotected.
+
+Now how to let our server know about api key and read it  ?
+
+    1- a third party library (.env)
+    install it (npm install dotenv)
+
+    2- create .env file 
+
+    3- requier dotenv
+*/
+
+// read dotenv file
+const dotenv =require("dotenv");
+
+//start (configer) the dotenv
+dotenv.config();
+//process.env >> built in fucn
+const APIKEY =process.env.APIKEY;
+// console.log(APIKEY);
+
+
+
+
+function Movie(title, overview, poster_path,id,release_date){
     this.title=title;
     this.overview=overview;
     this.poster_path=poster_path;
+    this.id=id;
+    this.release_date=release_date;
 }
 
 app.get("/",helloMovie);
 app.get("/favorit",hellofavorit);
+app.get("/trending",helloTrending);
+app.get("/search",hellosearch);
 app.get("*",notFoundHandler);
 
 app.use(errorHandler);
@@ -24,6 +56,9 @@ function errorHandler(error,req,res){
 
 function helloMovie(req ,res){
     let arr=[]
+    // arr.push(movies.title);
+    // arr.push(movies.overview);
+    // arr.push(movies.poster_path);
     movies.data.forEach((value) => {
         let theMovie = new Movie(value.title, value.overview, value.poster_path);
         arr.push(theMovie);
@@ -38,6 +73,39 @@ function hellofavorit(req,res){
  .catch(error =>{
     errorHandler("Sorry, something went wrong",req,res);
 })
+}
+
+function helloTrending(req,res){
+    let result = [];
+    
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`)
+   .then(apiResponse =>{
+       apiResponse.data.results.map(value =>{ // results>> is the array name in API link (and we shoud change it according to what API link array name)
+           let oneMoveDetails =new Movie(value.id,value.title,value.release_date,value.overview,value.poster_path)
+           result.push(oneMoveDetails);
+       })
+       return res.status(200).json(result);
+  }).catch(error =>{
+      errorHandler("Sorry, something went wrong",req,res);
+  })
+}
+
+function hellosearch(req,res){
+   const search ="The Royal Treatment,";
+    let Sresult = [];
+    
+    axios.get(` https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${search}`)
+   .then(apiResponse =>{
+       apiResponse.data.results.map(value =>{ // results>> is the array name in API link (and we shoud change it according to what API link array name)
+           let oneMoveSearch =new Movie(value.title || "A/N" ,value.overview || "A/N" ,value.release_date || "A/N" ,value.id || "A/N" ,value.release_date || "A/N")
+           Sresult.push(oneMoveSearch);
+       })
+       return res.status(200).json(Sresult);
+  }).catch(error =>{
+      errorHandler("Sorry, something went wrong",req,res);
+  })
+
+  
 }
 
 function notFoundHandler(req,res){
